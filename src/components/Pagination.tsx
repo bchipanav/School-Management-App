916a5633bf200e666b2a02ba32eb53681d047a5c
Brenda Chipana <brendachipanav@gmail.com>
2025-05-23 12:57:09 -0500

    feat: UI part 1

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Pagination = () => {
+	return (
+		<div className="p-4 flex items-center justify-between text-gray-500">
+			<button
+				type="button"
+				disabled
+				className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed"
+			>
+				Prev
+			</button>
+			<div className="flex items-center gap-2 text-sm">
+				<button type="button" className="px-2 rounded-sm bg-atioSky">
+					1
+				</button>
+				<button type="button" className="px-2 rounded-sm ">
+					2
+				</button>
+				<button type="button" className="px-2 rounded-sm ">
+					3
+				</button>
+				...
+				<button type="button" className="px-2 rounded-sm ">
+					10
+				</button>
+			</div>
+			<button
+				type="button"
+				className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed"
+			>
+				Next
+			</button>
+		</div>
+	);
+};
+
+export default Pagination;
---
+++ src/components/Performance.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+const Performance = () => {
+	return <div>Performance</div>;
+};
+
+export default Performance;
